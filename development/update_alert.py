5701a784e43fa8376e1d2916404620e0913681f7
darknessfalls <jesams2@gmail.com>
2023-10-16 16:58:44 -0500

    Update update_alert.py

--- development/update_alert.py
+++ development/update_alert.py
@@ -26,16 +26,16 @@ for root, dirs, files in os.walk("detections/"):
                 with open(full_path,"rb") as toml:
                     alert = tomllib.load(toml)
 
-                    # grabbing only the fields required by Elastic. Can be modified to include more fields.
-                    if alert['rule']['type'] == "query": # query based alert
-                        required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'threat']
-                    elif alert['rule']['type'] == "eql": # event correlation alert
-                        required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'language', 'threat']
-                    elif alert['rule']['type'] == "threshold": # threshold based alert
-                        required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'threshold', 'threat']
-                    else:
-                        print("Unsupported rule type found in: " + full_path)
-                        break
+                # grabbing only the fields required by Elastic. Can be modified to include more fields.
+                if alert['rule']['type'] == "query": # query based alert
+                    required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'threat']
+                elif alert['rule']['type'] == "eql": # event correlation alert
+                    required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'language', 'threat']
+                elif alert['rule']['type'] == "threshold": # threshold based alert
+                    required_fields = ['author', 'description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'threshold', 'threat']
+                else:
+                    print("Unsupported rule type found in: " + full_path)
+                    break
 
                 # Converter code and more formatting!
                 for field in alert['rule']:
@@ -49,14 +49,14 @@ for root, dirs, files in os.walk("detections/"):
                         elif type(alert['rule'][field]) == dict:
                             data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'", "\"") + "," + "\n"
             
-            data += " \"enabled\": true\n}" # finishing JSON syntax conversion with a newline (Enter) and closing curly braces
-        # print(data) # testing for "trouble" alerts when uploading to Elastic
+                data += " \"enabled\": true\n}" # finishing JSON syntax conversion with a newline (Enter) and closing curly braces
+            # print(data) # testing for "trouble" alerts when uploading to Elastic
 
-        rule_id = alert['rule']['rule_id']
-        url = url + "?rule_id=" + rule_id
+            rule_id = alert['rule']['rule_id']
+            url = url + "?rule_id=" + rule_id
 
-        elastic_data = requests.put(url, headers=headers, data=data).json()
-        print(elastic_data)
+            elastic_data = requests.put(url, headers=headers, data=data).json()
+            print(elastic_data)
 
 
 
